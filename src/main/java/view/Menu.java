ae2bb4414c3d22298273c7b6c944ad21b4d49763
Aleksey Dobrovolsky <a.dobrovolsky1991@gmail.com>
2019-07-07 15:09:28 +0300

    fix: remove an extra cycle in menu flow

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -19,37 +19,37 @@ public class Menu {
         String input;
 
         do {
-            do {
-                System.out.println("Select task number to execute:" +
-                        "\n1. Mobile rates hierarchy" +
-                        "\n2. Operations with collection" +
-                        "\n3. Operations with collection using stream api" +
-                        "\n4. Speed comparison of different implementations of List" +
-                        "\n5. Exit");
-                input = in.nextLine();
+            System.out.println("\nSelect task number to execute:" +
+                    "\n1. Mobile rates hierarchy" +
+                    "\n2. Operations with collection" +
+                    "\n3. Operations with collection using stream api" +
+                    "\n4. Speed comparison of different implementations of List" +
+                    "\n5. Exit");
+            input = in.nextLine();
 
-                switch (input) {
-                    case "1":
-                        new RateListRunner().run();
-                        break;
-                    case "2":
-                        new StudentsListRunner().runClassic();
-                        break;
-                    case "3":
-                        new StudentsListRunner().runStream();
-                        break;
-                    case "4":
-                        Lists.compare();
-                        break;
-                    case "5":
-                        break;
-                }
-                if (Objects.equals(input, "5")) break;
-
-                System.out.println("Exit? y/n");
-                input = in.nextLine();
-            } while (!Objects.equals(input, "y"));
-            if (Objects.equals(input, "y")) break;
+            switch (input) {
+                case "1":
+                    System.out.println("\n1. Mobile rates hierarchy task result");
+                    new RateListRunner().run();
+                    break;
+                case "2":
+                    System.out.println("\n2. Operations with collection task result");
+                    new StudentsListRunner().runClassic();
+                    break;
+                case "3":
+                    System.out.println("\n3. Operations with collection using stream api task result");
+                    new StudentsListRunner().runStream();
+                    break;
+                case "4":
+                    System.out.println("\n4. Speed comparison of different implementations of List task result");
+                    Lists.compare();
+                    break;
+                case "5":
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
         } while (!Objects.equals(input, "5"));
     }
 }
